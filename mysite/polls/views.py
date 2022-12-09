a8ac45a033e12cc1011ea6587b954c2e19b0e6e9
ZoyaAnsarii <118909858+ZoyaAnsarii@users.noreply.github.com>
2022-12-09 21:24:01 +0500

    Delete mysite directory

--- mysite/polls/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse("<h1>Polls App</h1>")
-def about(request):
-    return HttpResponse("<h1>Pakistan Zindabad!</h1>")
